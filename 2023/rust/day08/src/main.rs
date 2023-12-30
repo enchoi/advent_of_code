a02f79be9bbe2c5b13e6bb820f9a3daba683d14a
jason letort <enchoi@free.fr>
2023-12-30 18:28:13 +0900

    Day08 part2 Rust

--- 2023/rust/day08/src/main.rs
+++ 2023/rust/day08/src/main.rs
@@ -1,9 +1,10 @@
+use num::integer::div_floor;
 use std::collections::HashMap;
 
 fn main() {
     let data = include_str!("./input.txt");
     let (sequence, nodes_string) = parse_data(data);
-    compute_part1(sequence, nodes_string);
+    compute_part2(sequence, nodes_string);
 }
 
 fn parse_data(data: &str) -> (Vec<usize>, HashMap<String, Vec<String>>) {
@@ -38,4 +39,36 @@ fn compute_part1(sequence: Vec<usize>, nodes: HashMap<String, Vec<String>>) {
         index += 1;
     }
     println!("index: {}", index);
+}
+fn compute_part2(sequence: Vec<usize>, nodes: HashMap<String, Vec<String>>) {
+    let current = nodes
+        .keys()
+        .filter(|key| key.ends_with("A"))
+        .map(|node| node.to_string())
+        .collect::<Vec<String>>();
+
+    let number = current
+        .iter()
+        .map(|node| {
+            let mut test = node.to_string();
+            let mut index = 0;
+            while !test.ends_with("Z") {
+                test = nodes[&test][sequence[index % sequence.len()]].to_string();
+                index += 1;
+            }
+            index as i32
+        })
+        .collect::<Vec<i32>>();
+    println!(
+        "lcm: {}",
+        lcm(number.iter().map(|int| *int as u64).collect())
+    );
+}
+
+fn lcm(a: Vec<u64>) -> u64 {
+    let mut lcm = a[0];
+    for i in a.iter() {
+        lcm = div_floor(lcm * *i, num::integer::gcd(lcm, *i));
+    }
+    lcm
 }
